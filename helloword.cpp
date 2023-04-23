3e70f41dafc9d72bc010cb4d05a566b949ddf151
Tyler Herron <tylerherron7777@gmail.com>
2023-04-22 19:38:26 -0500

    clear

--- helloword.cpp
+++ helloword.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
-
+int clear()
+{
+    system("C:\\Code\\Repos\\c++\\clear.bat");
+    return 0;
+}
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg)
+    for (const string &word : msg)
     {
         cout << word << " ";
     }
     cout << endl;
+    clear();
 }
